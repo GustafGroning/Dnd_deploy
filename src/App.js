7894ef5917dc6a3c31ae148eeaec031d208f5a0e
Gustaf Gröning <gustafgroning@gmail.com>
2021-03-19 11:23:59 +0100

    remade branch

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+
+<div class="container">
+
+    <div class="header">
+      <img src="https://media.dnd.wizards.com/styles/second_hubpage_banner/public/images/head-banner/NEW-TO-DnD_What-is-DnD_Subsection_Hero_140718.jpg" />
+    </div>
+
+
+    <div class="bigpic">
+      <p class="font1"> INTRODUCTION TO ROLE-PLAYING </p>
+    </div>
+
+    <div class="races"> <p class="racestext"> RACES</p> </div>
+    <div class="classes">  <p class="classestext"> CLASSES </p>  </div>
+
+    <div class="magic"> <p class="magictext"> MAGIC </p> </div>
+
+
+    <div class="gods">
+      <p class="godstext"> GODS </p>
+    </div>
+
+    <div class="alignment"> <p class="alignmenttext"> ALIGNMENT </p> </div>
+
+    <div class="textruta1">
+      <h1 class="first-headline" > ABOUT THE GAME </h1>
+      <p class="first-text" > The first Dungeons & Dragons game was played back when Gary Gygax and Dave Arneson chose to personalize the massive battles of their fantasy wargames with the exploits of individual heroes. This inspiration became the first fantasy roleplaying game, in which players are characters in an ongoing fantasy story. This new kind of game has become immensely popular over the years, and D&D has grown to include many new ways to vividly experience worlds of heroic fantasy.
+    The core of D&D is storytelling. You and your friends tell a story together, guiding your heroes through quests for treasure, battles with deadly foes, daring rescues, courtly intrigue, and much more. You can also explore the world of Dungeons & Dragons through any of the novels written by its fantasy authors, as well as engaging board games and immersive video games. All of these stories are part of D&D. </p>
+
+    </div>
+
+
+    <div class="textruta2">
+      <h1 class="second-headline"> WHAT YOU NEED</h1>
+      <p class="second-text"> One of the great things about D&D is that you only really need a copy of the rules,  a set of dice, and your imagination. Wizards of the Coast put a PDF of the basic rules online for free because they “want to reduce the barrier to entry as much as possible,” according to communications director, Greg Tito.
+
+    D&D famously uses a bunch of weird-looking dice, which some players collect like they’re Pokémon cards. If you’re super into that aspect of the hobby, you can fall down an Etsy wormhole of unique handmade dice sets. (I’ve never been one of those players, but will admit that I backed a Kickstarter to get a set that looks like the bisexual pride flag.) For everyone else, a standard set of polyhedral dice from Amazon works just as well. The best place to start is with, well, the Starter Set. The box includes everything you need to get started: an essential rules handbook, an introductory adventure (which fans of The Adventure Zone might recognize from the first few episodes of the podcast), five pre-made character sheets, and a set of dice. </p> </div>
+
+
+
+
+
+    <p class="headertext"> DUNGEONS & DRAGONS </p>
+
+
+
+    <div class="footer">
+      <ul class="menys">
+
+        <li> CLASSES </li>
+        <li> RACES </li>
+        <li> GODS</li>
+        <li> MAGIC </li>
+        <li> ALIGNMENT</li>
+
+      </ul>
+    </div>
+
+
+
+
+
+
+    </div>
+
+
+  );
+}
+
+export default App;
